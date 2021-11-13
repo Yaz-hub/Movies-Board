7bed8bcbdef5c241c170f53c118556e28412b2f8
Yaz-hub <56580290+Yaz-hub@users.noreply.github.com>
2021-11-13 22:06:41 +0100

    App Js and styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+
 import "./App.css";
-import MoviesList from "./components/MoviesList"
 import "bootstrap/dist/css/bootstrap.min.css";
+import MoviesList from "./components/MoviesList"
 import MovieDetail from "./components/MovieDetail";
 import Navigation from "./components/Navigation";
+import AddMovie from "./components/AddMovie/AddMovie";
+import Footer from "./components/Footer/Footer";
 import {
   BrowserRouter,
   Routes,
@@ -12,18 +15,19 @@ import {
 
 function App() {
   return (
-    <div style={{ backgroundImage: `url("bg.jpeg")` }}>
+    <div>
       <Navigation />
       <div>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<MoviesList />} />
-            <Route path="movies" element={<MoviesList />} />
+            <Route path="/movies" element={<MoviesList />} />
+            <Route path="/add" element={<AddMovie />} />
             <Route path="/movies/:id" element={<MovieDetail/>} />
-
           </Routes>
         </BrowserRouter>
     </div>
+    <Footer></Footer>
     </div>
   );
 }
--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,4 +1,5 @@
 import http from "../http-common";
+import TMDB from "../TMDB";
 
 const getAll = () => {
   return http.get("/movies");
@@ -11,11 +12,19 @@ const remove = id => {
   return http.delete(`/movies/${id}`);
 };
 
+const serach = (text = '', year = null) => {
+  return TMDB.get(`/3/search/movie?api_key=32ce1364a5dbb3e2ec81dba08a7f228f&language=en-US&query=${text}&year=${year}`);
+};
+const create = (data) => {
+  return http.post(`/movies`, data);
+};
 
 export default {
     getAll,
     get,
     remove,
+    serach,
+    create,
 };
 
 
