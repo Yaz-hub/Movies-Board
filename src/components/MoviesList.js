bcb11d3a4376d17f6157c51fb2472a9b4582221f
Yaz-hub <56580290+Yaz-hub@users.noreply.github.com>
2021-11-13 22:00:23 +0100

    adding Movies List styles and updating component

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import MovieService from "../services/MovieService";
-import MovieCard from "../components/MovieCard/MovieCard"
-
+import MovieCard from "../components/MovieCard/MovieCard";
+import "./MoviesList.css";
+import { Col, Container, Row } from "react-bootstrap";
+import { Input } from "semantic-ui-react";
 
 const MoviesList = () => {
   const [movies, setMovies] = useState([]);
-  
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
   useEffect(() => {
     retrieveMovies();
   }, []);
@@ -25,16 +29,56 @@ const MoviesList = () => {
       });
   };
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = movies.filter((item) => {
+        if (item.title || item.categories || item.realease_date) {
+          return Object.values(item)
+          .join("")
+          .toLowerCase()
+          .includes(searchInput.toLowerCase());
+        }
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(movies);
+    }
+  };
+
   return (
-    <div className="col-md-12 bg-dark">
-      <div className="row justify-content-center">
-        <h4>Movies List</h4>
-        {movies &&
-          movies.map((movie, index) => (
-            <MovieCard key={movie.id} {...movie} handleRemoveMovie={handleRemoveMovie} />
-          ))}
-      </div>
-    </div>
+    <Container style={{ paddingTop: 10, paddingBottom: 10 }}>
+      <Input
+        icon="search"
+        placeholder="Search..."
+        onChange={(e) => searchItems(e.target.value)}
+      />
+      <h3 className="section-title">Movies List</h3>
+      <Row className="row-cols-1 row-cols-md-4">
+        {searchInput.length > 1
+          ? filteredResults.map((movie) => {
+              return (
+                <Col className="mb-4">
+                  <MovieCard
+                    key={movie.id}
+                    {...movie}
+                    handleRemoveMovie={handleRemoveMovie}
+                  />
+                </Col>
+              );
+            })
+          : movies &&
+            movies.map((movie) => (
+              <Col className="mb-4">
+                <MovieCard
+                  key={movie.id}
+                  {...movie}
+                  handleRemoveMovie={handleRemoveMovie}
+                />
+              </Col>
+            ))}
+      </Row>
+    </Container>
   );
 };
 
