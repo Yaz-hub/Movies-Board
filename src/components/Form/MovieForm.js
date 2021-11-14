92b7b9f511b79b073cee8305eb3e34fd761aa384
Yaz-hub <56580290+Yaz-hub@users.noreply.github.com>
2021-11-14 22:38:50 +0100

    Required fileds done

--- src/components/Form/MovieForm.js
+++ src/components/Form/MovieForm.js
@@ -5,59 +5,72 @@ import MovieService from "../../services/MovieService";
 import ActorField from "./ActorField";
 import CategoryInput from "./CategoryInput";
 import SimiliarMovieField from "./SimilarMovieField";
+import validator from 'validator'
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import "./MovieForm.css";
 
-const MovieFrom = ({selectdMovie}) => {
+const MovieFrom = ({ selectdMovie, editedMovie }) => {
+  const [validated, setValidated] = useState(false);
 
+  // set movie according to passed params (either selected or edited)
+  const [movie, setMovie] = useState(
+    editedMovie && editedMovie.id ? editedMovie : selectdMovie
+  );
+
+  const [errorMsg, setErrorMsg] = useState("");
+  const [releaseDate, setReleaseDate] = useState(new Date());
   const [categories, setCategories] = useState([]);
-  const [actors, setActors] = useState([{ name: "", photo: "", character: "" }]);
-  const [similarMovies, setSimilarMovies] = useState([{ title: "", photo: "", release_date: "" }]);
+  const [actors, setActors] = useState([{ name: "", photo: "", character: "" },]);
+  const [similarMovies, setSimilarMovies] = useState([{ title: "", poster: "", release_date: "" },]);
   const navigate = useNavigate();
-
-  const initialMovieState = {
-    title: "",
-    release_date: "",
-    categories: [],
-    description: "",
-    poster: "",
-    backdrop: "",
-    actors: [{}],
-    similar_movies: [{}],
-  };
 
   useEffect(() => {
     if (selectdMovie) {
       // automatically set values from the selected movie (auto-complete form)
       setMovie({
         title: selectdMovie.title,
-        release_date: selectdMovie.release_date,
+        release_date: selectdMovie.release_date
+          ? new Date(selectdMovie.release_date)
+          : "",
         description: selectdMovie.overview,
-        poster: selectdMovie.poster_path ? "https://image.tmdb.org/t/p/w342" + selectdMovie.poster_path  : "",
-        backdrop: selectdMovie.backdrop_path ? "https://image.tmdb.org/t/p/w342" + selectdMovie.backdrop_path : "",
+        poster: selectdMovie.poster_path
+          ? "https://image.tmdb.org/t/p/w342" + selectdMovie.poster_path
+          : "",
+        backdrop: selectdMovie.backdrop_path
+          ? "https://image.tmdb.org/t/p/w342" + selectdMovie.backdrop_path
+          : "",
+        actors: [{}],
+        similar_movies: [{}],
+        categories: [],
       });
     }
   }, [selectdMovie]);
 
-  const [movie, setMovie] = useState(initialMovieState);
-  const [errorMsg, setErrorMsg] = useState('');
-
   const handleCategoryChange = (items) => {
     setCategories(items);
-  }
+  };
+
+  const cleanArray = (items) => {
+    // clean empty object properties
+    let cleanedItems = items.map((element) => {
+      return Object.fromEntries(
+        Object.entries(element).filter(([_, v]) => v !== "")
+      );
+    });
+    // remove empty objects from array
+    return cleanedItems.filter((value) => Object.keys(value).length !== 0);
+  };
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
+    let error = [];
     switch (name) {
+      case 'poster':
       case 'backdrop':
-      case 'poster':  
-    //     try {
-    //       url = new URL(value);
-          setMovie((prevState) => ({
-            ...prevState,
-            [name]: value
-          }));
-    //     } catch (_) {
-    //       // silent 
-    //     }
+        if (!validator.isURL(value)) {
+          error[name] = 'Please enter a valid URL for ' + name;
+        }
         break;
       default:
         setMovie((prevState) => ({
@@ -65,117 +78,142 @@ const MovieFrom = ({selectdMovie}) => {
           [name]: value
         }));
     }
+    setErrorMsg(error);
   };
 
   const handleOnSubmit = (event) => {
     const formData = new FormData(event.currentTarget);
     event.preventDefault();
-    
+    console.log(errorMsg);
+    if (errorMsg !== "") {
+      return;
+    }
+
     const data = {};
     for (let [key, value] of formData.entries()) {
+      if (key.startsWith("actor.") || key.startsWith("sm.")) {
+        // skip posted data for actor. and sm.
+        // fields are handled in state (actors, similar_movies)
+        continue;
+      }
       data[key] = value;
     }
-    data['categories'] = categories;
-    data['actors'] = actors 
-    data['similar_movies'] = similarMovies;
-    MovieService.create(data);
+
+    if (releaseDate && releaseDate instanceof Date) {
+      // handle release_date format
+      let y = new Intl.DateTimeFormat("en", { year: "numeric" }).format(releaseDate);
+      let m = new Intl.DateTimeFormat("en", { month: "numeric" }).format(releaseDate);
+      let d = new Intl.DateTimeFormat("en", { day: "numeric" }).format(releaseDate);
+      data["release_date"] = `${y}-${m}-${d}`;
+    }
+    data["categories"] = categories;
+    data["actors"] = cleanArray(actors);
+    data["similar_movies"] = cleanArray(similarMovies);
+    if (editedMovie && editedMovie.id) {
+      MovieService.update(editedMovie.id, data);
+    } else {
+      MovieService.create(data);
+    }
     navigate("/");
-    
-    // const values = [title, release_date, description, backdrop, similar_movies, actors, poster];
-    // let errorMsg = '';
-
-    // const allFieldsFilled = values.every((field) => {
-    //   const value = `${field}`.trim();
-    //   return value !== '' && value !== '0';
-    // });
-
-    // if (allFieldsFilled) {
-    //   const movie = {
-    //     id: uuidv4(),
-    //     title,
-    //     release_date,
-    //     description,
-    //     actors,
-    //     poster,
-    //     backdrop,similar_movies
-    //   };
-    //   props.handleOnSubmit(movie);
-    // } else {
-    //   errorMsg = 'Please fill out all the fields.';
-    // }
-    // setErrorMsg(errorMsg);
   };
 
   return (
-    <Container>
+    <Container style={{ paddingTop: 10, paddingBottom: 10 }}>
       <Form onSubmit={handleOnSubmit}>
-      <Form.Group>
-        <Form.Label>Movie Title</Form.Label>
-        <Form.Control
-          className="input-control"
-          type="text"
-          name="title"
-          defaultValue={movie.title}
-          placeholder="Enter title of movie"
-          onChange={handleInputChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Realease Date</Form.Label>
-        <Form.Control
-          className="input-control"
-          type="text"
-          name="release_date"
-          defaultValue={movie.release_date}
-          placeholder="Enter Realease Date of movie"
-          onChange={handleInputChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Description</Form.Label>
-        <Form.Control
-          as="textarea"
-          className="input-control"
-          type="text"
-          name="description"
-          defaultValue={movie.description}
-          placeholder="Enter Movie descriotion"
-          onChange={handleInputChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Poster URL</Form.Label>
-        <Form.Control
-          className="input-control"
-          type="text"
-          name="poster"
-          defaultValue={movie.poster}
-          placeholder="Enter Poster URL"
-          onChange={handleInputChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Backdrop URL</Form.Label>
-        <Form.Control
-          className="input-control"
-          type="text"
-          name="backdrop"
-          defaultValue={movie.backdrop}
-          placeholder="Enter backdrop URL"
-          onChange={handleInputChange}
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Categories</Form.Label>
-        <CategoryInput tags={categories} handleChange={handleCategoryChange}></CategoryInput>
-      </Form.Group>
-      <Form.Group>
-        <ActorField actors={actors} setActors={setActors}></ActorField>
-        {/* <SimiliarMovieField similarMovies={similarMovies} setSimilarMovies={setSimilarMovies}></SimiliarMovieField> */}
-      </Form.Group>
-      <Button variant="primary" type="submit" className="submit-btn mt-3">
-        Submit
-      </Button>
+        <fieldset>
+          <legend>Movie Information</legend>
+          <Form.Group className="mb-3">
+            <Form.Label>Movie Title</Form.Label>
+            <Form.Control
+              required
+              className="input-control"
+              type="text"
+              name="title"
+              defaultValue={movie.title}
+              placeholder="Enter title of movie"
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Realease Date</Form.Label>
+            <DatePicker
+              dateFormat="dd/MM/yyyy"
+              selected={releaseDate}
+              onChange={(date) => setReleaseDate(date)}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              required
+              as="textarea"
+              rows={5}
+              className="input-control"
+              type="text"
+              name="description"
+              defaultValue={movie.description}
+              placeholder="Enter Movie descriotion"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Poster URL</Form.Label>
+            <Form.Control
+              className="input-control"
+              type="text"
+              name="poster"
+              defaultValue={movie.poster}
+              placeholder="Enter Poster URL"
+              onChange={handleInputChange}
+            />
+            <span style={{fontWeight: 'bold', color: 'red'}}>{errorMsg["poster"]}</span>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Backdrop URL</Form.Label>
+            <Form.Control
+              className="input-control"
+              type="text"
+              name="backdrop"
+              defaultValue={movie.backdrop}
+              placeholder="Enter backdrop URL"
+              onChange={handleInputChange}
+            />
+            <span style={{fontWeight: 'bold', color: 'red'}}>{errorMsg["backdrop"]}</span>
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Categories</Form.Label>
+            <CategoryInput
+              tags={movie.categories ? movie.categories : categories}
+              handleChange={handleCategoryChange}
+            ></CategoryInput>
+          </Form.Group>
+        </fieldset>
+        <fieldset>
+          <legend>Actors</legend>
+          <Form.Group className="mb-3">
+            <ActorField
+              actors={
+                movie.actors && movie.actors.length ? movie.actors : actors
+              }
+              setActors={setActors}
+            ></ActorField>
+          </Form.Group>
+        </fieldset>
+        <fieldset>
+          <legend>Similar Movies</legend>
+          <Form.Group className="mb-3">
+            <SimiliarMovieField
+              similarMovies={
+                movie.similar_movies && movie.similar_movies.length
+                  ? movie.similar_movies
+                  : similarMovies
+              }
+              setSimilarMovies={setSimilarMovies}
+            ></SimiliarMovieField>
+          </Form.Group>
+        </fieldset>
+        <Button variant="primary" type="submit" className="submit-btn mt-3">
+          Submit
+        </Button>
       </Form>
     </Container>
   );
