c6c00d4e6a6e9dedef2f691eb1c897bde3791f09
Yaz-hub <56580290+Yaz-hub@users.noreply.github.com>
2021-11-14 23:03:52 +0100

    restore search bar

--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -4,11 +4,14 @@ import MovieCard from "../MovieCard/MovieCard";
 import { Col, Container, Modal, Row } from "react-bootstrap";
 import ConfirmModal from "../Modal/ConfirmModal";
 import "./MoviesList.css";
+import { Input } from "semantic-ui-react";
 
 const MoviesList = () => {
   const [movies, setMovies] = useState([]);
   const [currentMovieId, setCurrentMovieId] = useState(null);
   const [show, setShow] = useState(false);
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
   useEffect(() => {
     retrieveMovies();
@@ -24,19 +27,61 @@ const MoviesList = () => {
       });
   };
 
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== "") {
+      const filteredData = movies.filter((item) => {
+        if (item.title || item.categories || item.realease_date) {
+          return Object.values(item)
+            .join("")
+            .toLowerCase()
+            .includes(searchInput.toLowerCase());
+        }
+      });
+      setFilteredResults(filteredData);
+    } else {
+      setFilteredResults(movies);
+    }
+  };
+
   return (
-      <Container style={{ paddingTop: 10, paddingBottom: 10 }}>
-          <h3 className="section-title">Movies List</h3>
-          <Row className="row-cols-1 row-cols-md-4">
-              {movies &&
-                movies.map((movie, index) => (
-                  <Col key={movie.id} className="mb-4">
-                    <MovieCard {...movie} setShow={setShow} setCurrentMovieId={setCurrentMovieId} />
-                  </Col>
-                ))}
-          </Row>
-          <ConfirmModal movieId={currentMovieId} show={show} setShow={setShow}></ConfirmModal>
-      </Container>
+    <Container style={{ paddingTop: 10, paddingBottom: 10 }}>
+      <Input
+        icon="search"
+        placeholder="Search..."
+        onChange={(e) => searchItems(e.target.value)}
+      />
+      <h3 className="section-title">Movies List</h3>
+      <Row className="row-cols-1 row-cols-md-4">
+        {searchInput.length > 1
+          ? filteredResults.map((movie) => {
+              return (
+                <Col className="mb-4">
+                  <MovieCard
+                    key={movie.id}
+                    {...movie}
+                    setShow={setShow} setCurrentMovieId={setCurrentMovieId}
+                  />
+                </Col>
+              );
+            })
+          : movies &&
+            movies.map((movie) => (
+              <Col className="mb-4">
+                <MovieCard
+                  key={movie.id}
+                  {...movie}
+                  setShow={setShow} setCurrentMovieId={setCurrentMovieId}
+                />
+              </Col>
+            ))}
+      </Row>
+      <ConfirmModal
+        movieId={currentMovieId}
+        show={show}
+        setShow={setShow}
+      ></ConfirmModal>
+    </Container>
   );
 };
 
