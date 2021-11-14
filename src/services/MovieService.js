760a3b90a0e7c96c6a84253f5b068ed0fed4ef6a
Yaz-hub <56580290+Yaz-hub@users.noreply.github.com>
2021-11-14 21:07:32 +0100

    add PUT method to apply editing

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,5 +1,4 @@
-import http from "../http-common";
-import TMDB from "../TMDB";
+import {http, TMDB} from "../http-common";
 
 const getAll = () => {
   return http.get("/movies");
@@ -15,16 +14,22 @@ const remove = id => {
 const serach = (text = '', year = null) => {
   return TMDB.get(`/3/search/movie?api_key=32ce1364a5dbb3e2ec81dba08a7f228f&language=en-US&query=${text}&year=${year}`);
 };
+
 const create = (data) => {
   return http.post(`/movies`, data);
 };
 
+const update = (id, data) => {
+  return http.put(`/movies/${id}`, data);
+};
 export default {
     getAll,
     get,
     remove,
     serach,
     create,
+    update
+    
 };
 
 
