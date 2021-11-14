fa4f3f7cb8d806b6d21063de2acd463bc37ee639
Yaz-hub <56580290+Yaz-hub@users.noreply.github.com>
2021-11-14 22:36:14 +0100

    error message fixed

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React from "react";
 
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import MoviesList from "./components/MoviesList"
-import MovieDetail from "./components/MovieDetail";
+import MoviesList from "./components/MoviesList/MoviesList"
 import Navigation from "./components/Navigation";
 import AddMovie from "./components/AddMovie/AddMovie";
 import Footer from "./components/Footer/Footer";
+import MovieDetail from "./components/MovieDetail/MovieDetail" 
 import {
   BrowserRouter,
   Routes,
   Route,
 } from "react-router-dom";
+import EditMovie from "./components/EditMovie";
 
 function App() {
   return (
-    <div>
+    <div id="page-container">
       <Navigation />
-      <div>
+      <div id="content-wrap">
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<MoviesList />} />
             <Route path="/movies" element={<MoviesList />} />
             <Route path="/add" element={<AddMovie />} />
-            <Route path="/movies/:id" element={<MovieDetail/>} />
+            <Route path="/movies/:id" element={<MovieDetail />} />
+            <Route path="/movies/:id/edit" element={<EditMovie />} />
           </Routes>
         </BrowserRouter>
-    </div>
-    <Footer></Footer>
+        <Footer></Footer>
+      </div>
     </div>
   );
 }
